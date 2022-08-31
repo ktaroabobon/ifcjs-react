a5aeb22cd03f5ae54802113a1bcefd6248c90b72
ktaroabobon <ktaroabobon@gmail.com>
2022-08-31 21:52:09 +0900

    add: vite.svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    root: "./src",
-    publicDir: "../public",
-    server: {
-        open: true,
-    },
-    plugins: [react()],
-    build: {
-        outDir: "../dist",
-        emptyOutDir: true,
-    }
+  base: "/ifcjs-react/",
+  root: "./src",
+  publicDir: "../public",
+  server: {
+    open: true,
+  },
+  plugins: [react()],
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
 });
