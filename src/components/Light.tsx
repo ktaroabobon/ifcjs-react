65b7fd4ae344854a641d86ccf334f329cafbc36d
ktaroabobon <ktaroabobon@gmail.com>
2022-09-01 22:43:32 +0900

    update: Light.tsx

---
+++ src/components/Light.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+export const Light: React.FC = () => {
+    const lightColor = 0xFFFFFF;
+
+    return (
+        <>
+            // 環境光源
+            <ambientLight
+                color={lightColor}
+                intensity={0.5}
+            />
+            // 平行光源
+            <directionalLight
+                color={lightColor}
+                position={[0, 10, 0]}
+                target-position={[-5, 0, 0]}
+            />
+        </>
+    );
+}
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,45 +1,46 @@
 import React from "react";
-import { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Mesh, AxesHelper } from "three";
-import { Controls } from "../components/Controls";
+import {useRef, useState} from "react";
+import {Canvas, useFrame} from "@react-three/fiber";
+import {Mesh, AxesHelper} from "three";
+import {Controls} from "../components/Controls";
+import {Light} from "../components/Light";
 
 type BoxProps = {
-  position: [x: number, y: number, z: number];
+    position: [x: number, y: number, z: number];
 };
 
 export const Box: React.FC<BoxProps> = (props) => {
-  const mesh = useRef<Mesh>(null!);
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  useFrame(() => (mesh.current.rotation.x += 0.01));
+    const mesh = useRef<Mesh>(null!);
+    const [hovered, setHover] = useState(false);
+    const [active, setActive] = useState(false);
+    useFrame(() => (mesh.current.rotation.x += 0.01));
 
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
+    return (
+        <mesh
+            {...props}
+            ref={mesh}
+            scale={active ? 1.5 : 1}
+            onClick={() => setActive(!active)}
+            onPointerOver={() => setHover(true)}
+            onPointerOut={() => setHover(false)}
+        >
+            <boxGeometry args={[1, 1, 1]}/>
+            <meshStandardMaterial color={hovered ? "hotpink" : "orange"}/>
+        </mesh>
+    );
 };
 
 export const Index: React.FC = () => {
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas>
-        <Controls />
-        <ambientLight />
-        <primitive object={new AxesHelper(10)} />
-        <pointLight position={[0, 0, 0]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
-    </div>
-  );
+    return (
+        <div style={{width: "100vw", height: "100vh"}}>
+            <Canvas>
+                <Controls/>
+                <Light/>
+                <primitive object={new AxesHelper(10)}/>
+                <pointLight position={[0, 0, 0]}/>
+                <Box position={[-1.2, 0, 0]}/>
+                <Box position={[1.2, 0, 0]}/>
+            </Canvas>
+        </div>
+    );
 };
