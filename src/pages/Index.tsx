8de8e5b5a855076c97f0216c950cc0f29e211fce
ktaroabobon <ktaroabobon@gmail.com>
2022-09-07 11:48:11 +0900

    add: IfcContainer.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,46 +1,55 @@
-import { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Mesh } from "three";
-import { Controls } from "../components/Controls/Controls";
-import { Light } from "../components/Light/Light";
-import { Helper } from "../components/Helper/Helper";
-import { FileInput } from "../components/FileInput/FileInput";
-
-type BoxProps = {
-  position: [x: number, y: number, z: number];
-};
-
-export const Box: React.FC<BoxProps> = (props) => {
-  const mesh = useRef<Mesh>(null!);
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  useFrame(() => (mesh.current.rotation.x += 0.01));
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-};
+import React, {createRef, useEffect, useRef, useState} from "react";
+import {extend} from "@react-three/fiber";
+import {Canvas, useFrame} from "@react-three/fiber"
+import {Mesh} from "three";
+import {Controls} from "../components/Controls/Controls";
+import {Light} from "../components/Light/Light";
+import {Helper} from "../components/Helper/Helper";
+import {FileInput} from "../components/FileInput/FileInput";
+import {Backdrop, CircularProgress} from "@mui/material";
+import {IfcContainer} from "../components/IFCContainer/IfcContainer";
+import {IfcViewerAPI} from "web-ifc-viewer";
 
 export const Index: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+
+  const ifcContainerRef = createRef<HTMLDivElement>();
+  const [ifcViewer, setIfcViewer] = useState<IfcViewerAPI>();
+
+  useEffect(() => {
+    if (ifcContainerRef.current) {
+      const container = ifcContainerRef.current;
+      const ifcViewer = new IfcViewerAPI({container: container});
+      ifcViewer.IFC.loader.ifcManager.applyWebIfcConfig({
+        COORDINATE_TO_ORIGIN: true,
+        USE_FAST_BOOLS: false
+      });
+      setIfcViewer(ifcViewer);
+    }
+  }, []);
+
+
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <FileInput />
+    <div style={{width: "100vw", height: "100vh"}}>
+      <FileInput/>
       <Canvas>
-        <Controls />
-        <Light />
-        <Helper />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
+        <Controls/>
+        <Light/>
+        <Helper/>
       </Canvas>
+      <IfcContainer ref={ifcContainerRef} viewer={ifcViewer}/>
+      <Backdrop
+        style={{
+          zIndex: 100,
+          display: "flex",
+          alignItems: "center",
+          alignContent: "center"
+        }}
+        open={loading}
+      >
+        <CircularProgress/>
+      </Backdrop>
     </div>
+
   );
 };
